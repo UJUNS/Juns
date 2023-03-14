b3740a0771451bccf22406051ca30d085382ee01
UJUNS <tjr4756@naver.com>
2023-03-14 09:58:31 +0900

    second commit

--- src/test_git/hello.java
+++ src/test_git/hello.java
@@ -5,6 +5,11 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
+		System.out.println("hello2");
+		System.out.println("hello3");
+		System.out.println("hello4");
+		System.out.println("hello5");
+		
 
 	}
 
